08a4f86a4b3953b6795d88327dacacb57b6d7414
Mark Simpkins <mark@maahee.com>
2011-02-17 17:05:06 +0000

    created Rails 3 framework app, nothing yet configured.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendlycrowds::Application.config.secret_token = '078018a08aa3aba54f70de15962a4d9bbf96c686cda7c22de5b96c3d457677b31bc6a272f8fa0f7048a946d70426a6174a08aeed6d3ee034215372e7b4836a3f'
