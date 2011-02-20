e9974a85a3f547566e90a95a7d5f0e2040e3e327
Mark Simpkins <mark@maahee.com>
2011-02-20 00:59:34 +0000

    added models for Questions and Users (including scaffolding for Questions). Also added Twitter based authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Friendlycrowds::Application.routes.draw do
+  resources :questions
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +51,12 @@ Friendlycrowds::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-
+  root :to => "questions#index"
+  
+  # Match the callbacks for authentication
+  match "/auth/:provider/callback" => "sessions#create"
+  match "/signout" => "sessions#destroy", :as => :signout
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
